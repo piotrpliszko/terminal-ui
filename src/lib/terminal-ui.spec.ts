626d541ff31e8439431abd4c315360910376d53b
Piotr Pliszko <pliszko.piotr@gmail.com>
2021-05-02 18:15:33 +0200

    feat: terminalui tests

--- src/lib/terminal-ui.spec.ts
+++ src/lib/terminal-ui.spec.ts
@@ -1,11 +1,74 @@
-// import { double, power } from '..';
+import { terminalui } from './terminal-ui';
 
-// describe('Number', () => {
-//   it('should double number', () => {
-//     expect(double(2)).toEqual(4);
-//   });
+const doc = `
+<div class="terminalui-container" data-terminalui>
+  <div data-terminalui-command>echo "test"</div>
+  <div data-terminalui-output>test</div>
+</div>
+<div data-terminalui>
+  <div data-terminalui-command>echo "test"</div>
+  <div data-terminalui-output>test</div>
+</div>`;
 
-//   it('should compute power', () => {
-//     expect(power(2, 4)).toEqual(16);
-//   });
-// });
+describe('terminal-ui', () => {
+  it('should return terminal when provided with no options', () => {
+    document.body.innerHTML = doc;
+
+    const terminal = terminalui();
+
+    expect(terminal).not.toBeUndefined();
+    expect(terminal).not.toBeNull();
+  });
+
+  it('should return terminal when provided with empty options object', () => {
+    document.body.innerHTML = doc;
+
+    const terminal = terminalui({});
+
+    expect(terminal).not.toBeUndefined();
+    expect(terminal).not.toBeNull();
+  });
+
+  it('should return terminal when provided with correct selector option', () => {
+    document.body.innerHTML = doc;
+
+    const terminal = terminalui({
+      selector: '.terminalui-container',
+    });
+
+    expect(terminal).not.toBeUndefined();
+    expect(terminal).not.toBeNull();
+  });
+
+  it('should find only matching objects when custom selector provided', () => {
+    document.body.innerHTML = doc;
+
+    const terminal = terminalui({
+      selector: '.terminalui-container',
+    });
+
+    expect(terminal).not.toBeUndefined();
+    expect(terminal).not.toBeNull();
+    expect(terminal.containers.length).toEqual(1);
+  });
+
+  it('should find all TerminalUI objects when no custom selector provided', () => {
+    document.body.innerHTML = doc;
+
+    const terminal = terminalui();
+
+    expect(terminal).not.toBeUndefined();
+    expect(terminal).not.toBeNull();
+    expect(terminal.containers.length).toEqual(2);
+  });
+
+  it('should throw error when no matching objects are found', () => {
+    document.body.innerHTML = doc;
+
+    expect(() => {
+      terminalui({
+        selector: '.no-matches',
+      });
+    }).toThrow('No TerminalUI objects found');
+  });
+});
